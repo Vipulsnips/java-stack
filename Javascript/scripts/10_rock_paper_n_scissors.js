c5ce1587ea0bb16d3efd837ae7be0496bb314b03
Vipulsnips <vipulrawat137@gmail.com>
2025-12-14 12:48:43 +0530

    dom and added css to projects

---
+++ Javascript/scripts/10_rock_paper_n_scissors.js
@@ -0,0 +1,56 @@
+    let score=JSON.parse(localStorage.getItem('score')) || {
+      wins:0,
+        losses:0,
+        ties:0
+      };
+      console.log(localStorage.getItem('score'));
+      // if(!score){
+    //   score={
+    //     wins:0,
+    //     losses:0,
+    //     ties:0
+    //   }
+    // }
+    function updatescore(){
+      document.querySelector('.js-score')
+      .innerHTML =` wins:${score.wins}, Ties:${score.ties}, Losses:${score.losses}`;
+    }
+    function pickMove(){
+      let choice='';
+      const value=Math.random();
+      if(value >=0 && value <(1/3)) choice='Rock';
+      else if(value < (2/3)) choice='Paper';
+      else choice='Scissors';
+      return choice;
+    }
+    function resultEval(playermove){
+      const choice= pickMove();
+      let result='';
+      if(playermove=== 'Scissors'){
+        if(choice==='Rock') result='Lose';
+        else if(choice === 'Paper') result='Win';
+        else result='Tie';
+      }
+      else if(playermove=== 'Paper'){
+        if(choice==='Rock') result='Win';
+        else if(choice === 'Paper') result='Tie';
+        else result='Lost';
+      }
+      else{
+        if(choice==='Rock') result='Tie';
+        else if(choice === 'Paper') result='Lost';
+        else result='Win';
+      }
+      if(result==='Win') score.wins++;
+      else if(result=== 'Tie') score.ties++;
+      else score.losses++;
+      localStorage.setItem('score',JSON.stringify(score));
+      updatescore();
+      document.querySelector('.js-result')
+        .innerHTML=result;
+      document.querySelector('.js-choice')
+        .innerHTML=`  you <img src="images/${playermove}-emoji.png" class="move-img"> <img src="images/${choice}-emoji.png" class="move-img">  computer`;
+    }
+    
+    document.querySelector('.js-score')
+    .innerHTML =` wins:${score.wins}, Ties:${score.ties}, Losses:${score.losses}`;
--- Javascript/variables.js
+++
@@ -1,4 +0,0 @@
-alert("hello"); 
-console.log(2+19);
-console.log('help' + 'me');
-console.log("help" + "me");
